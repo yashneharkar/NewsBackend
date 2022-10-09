aa0bff54cbd89de477d2bd25cef76bdd9022dd5a
yashneharkar <yashneharkar2003@gmail.com>
2022-10-09 13:47:28 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reactStrictMode: true,
+  swcMinify: true,
+  reactStrictMode: false,
+
+}
+
+module.exports = nextConfig
---
+++ pages/api/index.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+
+    res.status(200).json(
+        {
+        "munews":[
+        {
+            title: "Mumbai University to conduct winter session exams after Diwali, new dates to be out soon",
+            desc: "The University of Mumbai has decided to hold fifth-semester exams after Diwali following multiple requests from final-year students.  Previously, the tests were scheduled to begin on October 15.The Board of Examination and Evaluation, in a meeting, has decided to postpone the semester V exams till after Diwali. The students will be informed of the new examination dates before Diwali ends,” a university official said, as reported by news agency PTI.Once the authorities share the examination schedule, students will be able to download it via the official website - mu.ac.in.This comes after students from at least five city colleges complained in writing to Mumbai University that they had insufficient time and opportunity to prepare for a written exam in long-form format.According to a The Free Press Journal report, students claimed to have taken multiple-choice questions in online tests for the previous two years in a row. They, therefore, need more time to study for final exams that will also include subjective questions.",
+            imgUrl: "https://images.cnbctv18.com/wp-content/uploads/2020/07/exams-1019x573.jpg?impolicy=website&width=500&height=300"       
+        },
+        {
+            title: "MHT CET counselling provisional merit list released at cetcell.mahacet.org",
+            desc: "MHT CET counselling: The Maharashtra Common Entrance Test Cell has released the provisional merit list for MHT CET counselling 2022 for Maharashtra state/all India candidates.Candidates who registered for the counselling process, can now check the provisional merit list on the official website, cetcell.mahacet.org or fe2022.mahacet.org“All the eligible candidates who have submitted Application Form on or before 04-10-2022 Up to 04:00 PM are considered for the Provisional Merit List and assigned a Merit Number” reads the official notification.To check a candidate's provisional merit status candidates will have to enter their application ID and date of birth.CET Cell will release the round 1 final merit list and seat matrix on October 12, 2022.In case of any grievance, candidates can submit objections from October 8 to 10, 2022 upto 5 pm.CAP round 1 will begin from October 13 and will be concluded on October 15, 2022.",
+            imgUrl: "https://images.hindustantimes.com/img/2022/10/07/550x309/MHT_CET_1665130148824_1665130156739_1665130156739.PNG"
+        },
+        {
+            title: "MHT CET Counselling 2022: Registration on CAP portal ends tomorrow",
+            desc: "LMHT CET Counselling 2022: Registration for Maharashtra Common Entrance Test or MHT CET 2022 counselling ends tomorrow, October 4.Candidates who want to apply for admission to BE/BTech (4 year) and Master of Engineering (5 year integrated) courses through MHT CET can submit their application forms on cetcell.mahacet.org or fe2022.mahacet.org.As per the counselling schedule, the last date to submit and verify applications and documents is October 4 (4 pm). However, the facility for non-CAP candidates will continue till November 17 (5 pm).CET Cell will publish the provisional merit list for Maharashtra state/all India candidates on October 7 and the final merit list for round 1 on October 12, along with seat matrix.After that, candidates can choose and confirm options for CAP round 1 from October 13 to 15. The provisional allotment list for the first round will be available on October 18.Selected candidates have to accept seats from October 19 to 21 (3 pm) and report to the allotted institute and confirm admission from October 19 to 21.",
+            imgUrl: "https://images.hindustantimes.com/img/2022/10/03/550x309/_7bb2f206-ed27-11ea-92df-080f51a06faf_1664781310683_1664781310683.jpg"
+        }
+    ]});
+
+}

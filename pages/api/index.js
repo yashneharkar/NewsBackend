524e20b9882583ab56be7f6a178378fc31f0a7ab
yashneharkar <yashneharkar2003@gmail.com>
2022-10-11 11:16:01 +0530

    news update

--- pages/api/index.js
+++ pages/api/index.js
@@ -5,12 +5,12 @@ export default async function handler(req, res) {
         "munews":[
         {
             title: "Mumbai University to conduct winter session exams after Diwali, new dates to be out soon",
-            desc: "The University of Mumbai has decided to hold fif to begin on October 15.The Board of Examination and Evaluation, in a meeting, has decided to postpone the semester V exams till after Diwali. The students will be informed of the new examination dates before Diwali ends,” a university official said, as reported by news agency PTI.Once the authorities share the examination schedule, students will be able to download it via the official website - mu.ac.in.",
-            imgUrl: "https://images.cnbctv18.com/wp-content/uploads/2020/07/exams-1019x573.jpg?impolicy=website&width=500&height=300"       r
+            desc: "The University of Mumbai has decided to hold fifth-semester exams after Diwali following multiple requests from final-year students. Previously, the tests were scheduled to begin on October 15.The Board of Examination and Evaluation, in a meeting, has decided to postpone the semester V exams till after Diwali. The students will be informed of the new examination dates before Diwali ends,” a university official said, as reported by news agency PTI.Once the authorities share the examination schedule, students will be able to download it via the official website - mu.ac.in.",
+            imgUrl: "https://images.cnbctv18.com/wp-content/uploads/2020/07/exams-1019x573.jpg?impolicy=website&width=500&height=300"       
         },
-        {th-semester exams after Diwali following multiple requests from final-year students. Previously, the tests were scheduled
+        {
             title: "MHT CET counselling provisional merit list released at cetcell.mahacet.org",
-            desc: "MHT CET counselling: The Maharashtra Common Entrance Test Cell has released the provisional merit list for MHT CET counselling 2022 for Maharashtra state/all India candidates.Candidates who registered for the counselling process, can now check the provisional merit list on the official website, cetcell.mahacet.org or fe2022.mahacet.org“All the eligible candidates who have submitted Application Form on or before 04-10-2022 Up to 04:00 PM are considered for the Provisional Merit List and assigned a Merit Number” reads the official notification.To check a candidate's provisional merit status candidates will have to enter their application ID and date of birth.CET Cell will release the round 1 final merit list and seat matrix on October 12, 2022.In case of any grievance, candidates can submit objections from October 8 to 10, 2022 upto 5 pm.CAP round 1 will begin from October 13 and will be concluded on October 15, 2022.",
+            desc: "MHT CET counselling: Now check the provisional merit list on the official website, cetcell.mahacet.org or fe2022.mahacet.org“All the eligible candidates who have submitted Application Form on or before 04-10-2022 Up to 04:00 PM are considered for the Provisional Merit List and assigned a Merit Number” reads the official notification.CET Cell will release the round 1 final merit list and seat matrix on October 12, 2022.In case of any grievance, candidates can submit objections from October 8 to 10, 2022 upto 5 pm.CAP round 1 will begin from October 13 and will be concluded on October 15, 2022.",
             imgUrl: "https://images.hindustantimes.com/img/2022/10/07/550x309/MHT_CET_1665130148824_1665130156739_1665130156739.PNG"
         },
         {
@@ -19,8 +19,8 @@ export default async function handler(req, res) {
             imgUrl: "https://images.hindustantimes.com/img/2022/10/03/550x309/_7bb2f206-ed27-11ea-92df-080f51a06faf_1664781310683_1664781310683.jpg"
         },
         {
-            title: "ready proje t ",
-            desc: "any thimg nkjnffjkskjdf.",
+            title: "ready project ",
+            desc: "any thimg write it poperly.",
             imgUrl: "https://images.hindustantimes.com/img/2022/10/03/550x309/_7bb2f206-ed27-11ea-92df-080f51a06faf_1664781310683_1664781310683.jpg"
         }
     ]});
